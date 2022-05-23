d11181eb3a273455eefc4243462647f1af8f4a70
hyay <hya.chaigne@hotmail.fr>
2022-05-23 10:29:08 +0200

    Comments

--- display/doGraph.py
+++ display/doGraph.py
@@ -22,10 +22,29 @@ args = my_parser.parse_args()
 
 
 def x_coord(ac, bc, ab):
+    """
+    X coord calculation
+    Args:
+        ac:
+        bc:
+        ab:
+
+    Returns:
+
+    """
     return (math.pow(ac, 2) - math.pow(bc, 2) + math.pow(ab, 2)) / (2 * ab)
 
 
 def y_coord(ac, c_x):
+    """
+    Y coord calculation
+    Args:
+        ac:
+        c_x:
+
+    Returns:
+
+    """
     tmp = (math.pow(ac, 2) - math.pow(c_x, 2))
     if tmp > 0:
         return math.sqrt(tmp)
@@ -37,17 +56,36 @@ def y_coord(ac, c_x):
 
 
 def dist(ax, bx, ay, by):
+    """
+    Simple distance calculation
+    Args:
+        ax:
+        bx:
+        ay:
+        by:
+
+    Returns:
+
+    """
     return math.sqrt(math.pow((bx - ax), 2) + math.pow((by - ay), 2))
 
 
 def build_init_coordinates(coord_object):
+    """
+    Create 2 first base points, will be used as reference for every other point to calculate
+    Args:
+        coord_object:
+
+    Returns:
+
+    """
     # cluster1 {0,0}
     # cluster2 {?,0}
     x_points = [0]
     y_points = [0, 0]
 
     # init base segment
-    x_points.append(float(coord_object[0]["distancesReferences"]["B"])*10)
+    x_points.append(float(coord_object[0]["distancesReferences"]["B"]) * 10)
 
     # take values
     bx = x_points[len(x_points) - 1]
@@ -56,34 +94,24 @@ def build_init_coordinates(coord_object):
     ay = y_points[len(y_points) - 2]
     ab = dist(ax, bx, ay, by)
 
-    return ab, x_points, y_points
-
-    """ignore_first = False
-    ignore_second = False
-    for entry in coord_object.values():
-        # ignore first
-        if ignore_first and ignore_second:
-            ac = entry.get("A") * 10
-            bc = entry.get("B") * 10
-            cx = x_coord(ac, bc, ab)
-            cy = y_coord(ac, x_coord(ac, bc, ab))
-            x_points.append(cx)
-            y_points.append(cy)
-
-            # print("cx: ", cx)
-            # print("cy: ", cy)
-        else:
-            if ignore_first:
-                ignore_second = True
-            ignore_first = True"""
-
-    return np.fromiter(x_points, dtype=int), np.fromiter(y_points, dtype=int)
+    return ab
 
 
 def process_entry(entry, color, ab):
+    """
+    Process a cluster in json
+    Args:
+        entry:
+        color:
+        ab:
+
+    Returns:
+
+    """
+    # Gather needed infos
     name, distances, points = entry["name"], entry["distancesReferences"], entry["listePoints"]
+
     # First point
-    print(distances)
     ac = float(distances.get("A")) * 10
     bc = float(distances.get("B")) * 10
     cx = x_coord(ac, bc, ab)
@@ -95,7 +123,6 @@ def process_entry(entry, color, ab):
 
     # Side points
     for entry_point in points.values():
-        print(entry_point)
         ac = float(entry_point.get("A")) * 10
         bc = float(entry_point.get("B")) * 10
         cx = x_coord(ac, bc, ab)
@@ -108,21 +135,33 @@ def process_entry(entry, color, ab):
 
 
 def open_json(file):
+    """
+    Open JSON and process data
+    Args:
+        file:
+
+    Returns:
+
+    """
     try:
         f = open(file)
         data = json.load(f)
-        color = None
-        ab, x_points, y_points = build_init_coordinates(data['clusters'])
+
+        # Init coordinates
+        ab = build_init_coordinates(data['clusters'])
 
         for entry in data['clusters']:
+            # Distant color for each cluster
             color = "#" + ''.join([random.choice('ABCDEF0123456789') for i in range(6)])
+            # Do math
             process_entry(entry, color, ab)
         f.close()
     finally:
         f.close()
 
-
+# Given json in parameter
 if args.file:
+    # plt display config
     open_json(args.file)
     plt.axis([-5, 15, -5, 15])
     plt.axis("equal")
